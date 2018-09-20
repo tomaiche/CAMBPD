ab744e302b8b2bc39868f85ab36803ecb85b9a94
Tom <thomas_aiche@fr.ibm.com>
2018-09-20 15:10:37 +0000

    remove cloud connection variables

--- project2/variables.tf
+++ project2/variables.tf
@@ -4,20 +4,6 @@
 ##
 #####################################################################
 
-variable "ibm_bmx_api_key" {
-  type = "string"
-  description = "Generated"
-}
-
-variable "ibm_sl_username" {
-  type = "string"
-  description = "Generated"
-}
-
-variable "ibm_sl_api_key" {
-  type = "string"
-  description = "Generated"
-}
 
 variable "datacenter" {
   type = "string"
